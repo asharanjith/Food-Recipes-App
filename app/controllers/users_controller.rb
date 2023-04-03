4c82b3d5f727804629f5a016751e6943e487334a
asharanjith <ashasvijayan@gmail.com>
2023-04-03 09:06:18 +0400

    Set up navbar and alert using bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
 
     private
         def user_params
-            params.require(:user).permit(:name, :email, :password, :password_confirmation)
+            params.require(:user).permit(:name, :email, :password)
         end
         
 end
