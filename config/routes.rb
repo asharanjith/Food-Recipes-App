2a434e21dbb3db498f9eabf952debd6e30b94889
asharanjith <ashasvijayan@gmail.com>
2023-04-03 08:50:16 +0400

    Set up devise gem for login and user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'users#index'
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :users
 end
