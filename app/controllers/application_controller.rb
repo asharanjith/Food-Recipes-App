1afab1bf1b98e4c42d02a64d0d979794ba19764b
asharanjith <ashasvijayan@gmail.com>
2023-04-03 12:24:48 +0400

    Fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :update_allowed_parameters, if: :devise_controller?
 
-
   protected
 
   def update_allowed_parameters
