7bc96962d3aa12cdbfea0d0f839dba300940356d
asharanjith <ashasvijayan@gmail.com>
2023-04-03 09:13:56 +0400

    Add cancancan gem for authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
 
     before_action :authenticate_user!
     before_action :update_allowed_parameters, if: :devise_controller?
+    include CanCan::ControllerAdditions
+
+    rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, alert: exception.message
+  end
   
     protected
   
