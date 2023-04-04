cb4960e78230d532e95b8abb497dfb6366f1da3d
AhmadiGu <ahmadigulalai76@gmail.com>
2023-04-04 18:32:37 +0500

    confilect solved

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,10 +6,10 @@ class Ability
     #
     return unless user.present?
 
-    can :read, :all 
+    can :read, :all
     can :manage, Food, user_id: user.id
     can :manage, Recipe, user_id: user.id
- 
+
     #   return unless user.admin?
     # can :manage, :all
     #
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,13 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   resources :users 
-  resources :foods  
-  resources :recipes 
+  resources :foods
+  resources :recipes do
+    resources :recipe_foods, only: [:new, :create, :destroy]
+  end
+   
+  get '/shopping_list/:recipe_id', to: 'shopping_list#index', as: 'shopping_list'
+  get '/shopping_list', to: 'shopping_list#show', as: 'general_shopping_list
 
   put '/recipes/:id/toggle_privacy', to: 'recipes#toggle_privacy', as: 'toggle_recipe_privacy'
  
