b6c9631ea13021bba37bd734b2db93ccc640e9c3
AhmadiGu <ahmadigulalai76@gmail.com>
2023-04-04 12:51:25 +0500

    Linters errors fixed

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,10 +4,10 @@ class Ability
   def initialize(user)
     # Define abilities for the user here. For example:
     #
-    return unless user.present? 
+    return unless user.present?
 
     can :read, :all
-    can :manage, Food, user_id: user.id 
+    can :manage, Food, user_id: user.id
     can :manage, Recipe, user_id: user.id
     #   return unless user.admin?
     # can :manage, :all
