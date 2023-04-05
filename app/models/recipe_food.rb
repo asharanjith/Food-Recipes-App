dbc317bbb4a87bb3d5b88a4f9a42a1ed49d8f450
AhmadiGu <ahmadigulalai76@gmail.com>
2023-04-05 10:09:22 +0500

    RecipeFood model modified

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,7 +1,6 @@
 class RecipeFood < ApplicationRecord
   belongs_to :food
   belongs_to :recipe
-  has_many :recipes, through: :recipe_foods
 
   validates :quantity, numericality: { only_integer: true, greater_than: 0 }
 end
