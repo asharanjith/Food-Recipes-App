9717849d0f021c447ee113a2e243e2d6c9a345df
AhmadiGu <ahmadigulalai76@gmail.com>
2023-04-05 10:56:10 +0500

    Some files modifed

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,7 +1,7 @@
 class RecipeFoodsController < ApplicationController
   def new
     @recipe_food = RecipeFood.new
-    @recipe_id = params[:recipes_id]
+    @recipe_id = params[:recipe_id]
     @foods = current_user.foods
   end
 
@@ -9,7 +9,7 @@ class RecipeFoodsController < ApplicationController
     @recipe_food = RecipeFood.new(params_recipe_foods)
 
     if @recipe_food.save
-      redirect_to recipe_path(params[:recipes_id])
+      redirect_to recipe_path(params[:recipe_id])
     else
       render 'new'
     end
@@ -18,12 +18,12 @@ class RecipeFoodsController < ApplicationController
   def destroy
     @recipe_food = RecipeFood.find(params[:id])
     @recipe_food.destroy
-    redirect_to recipe_path(@recipe_food.recipes_id), notice: 'Recipe food was successfully deleted.'
+    redirect_to recipe_path(@recipe_food.recipe_id), notice: 'Recipe food was successfully deleted.'
   end
 
   private
 
   def params_recipe_foods
-    params.require(:recipe_food).permit(:quantity, :foods_id, :recipes_id)
+    params.require(:recipe_food).permit(:quantity, :foods_id, :recipe_id)
   end
 end
