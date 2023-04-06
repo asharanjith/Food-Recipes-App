dda060d39ce39900cbab14b2f55fddaab0d9a9ba
AhmadiGu <ahmadigulalai76@gmail.com>
2023-04-06 18:39:08 +0500

    Integration tests for food and shopping_list added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   #  :lockable, :timeoutable, :trackable and :omniauthable
 
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :foods, foreign_key: 'user_id', dependent: :destroy
